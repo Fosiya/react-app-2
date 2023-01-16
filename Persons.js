9d13405140c8a722dadf526cdea1ebbca0a9fe28
Fosiya <112564503+Fosiya@users.noreply.github.com>
2023-01-16 04:18:23 +0100

    Add files via upload

---
+++ Persons.js
@@ -0,0 +1,12 @@
+import React from "react"
+export default function Persons({ persons }){
+    const { name, order } = persons
+
+    return(
+        <li>
+            <h4>{name}</h4>
+            <p>Order: {order}</p>
+        </li>
+
+    )
+}
